cb8183a9bff11bce213099c437e2fb6a445a2c45
David DiGioia <davidofjoy@gmail.com>
2019-07-18 23:24:51 -0400

    use sampler to store info for reading texture

--- Penguin/src/Texture.cpp
+++ Penguin/src/Texture.cpp
@@ -4,18 +4,26 @@
 #include "stb_image/stb_image.h"
 
 Texture::Texture(const char* path)
-	: m_id{ 0 }, m_width{ 0 }, m_height{ 0 }, m_bitsPerPixel{ 0 }, m_buffer{ nullptr }
+	: m_textureID{ 0 }, m_width{ 0 }, m_height{ 0 }, m_bitsPerPixel{ 0 }, m_buffer{ nullptr }
 {
 	stbi_set_flip_vertically_on_load(1);
 	m_buffer = stbi_load(path, &m_width, &m_height, &m_bitsPerPixel, 4);
 
-	glGenTextures(1, &m_id);
-	glBindTexture(GL_TEXTURE_2D, m_id);
+	glGenTextures(1, &m_textureID);
+	glBindTexture(GL_TEXTURE_2D, m_textureID);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);   // horizontal wrap
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);   // vertical wrap
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);   // horizontal wrap
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);   // vertical wrap
+
+	// We're storing the information on how to read out texture in the sampler instead
+	// of in the texture itself
+	glGenSamplers(1, &m_samplerID);
+	glSamplerParameteri(m_samplerID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glSamplerParameteri(m_samplerID, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glSamplerParameteri(m_samplerID, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glBindSampler(0, m_samplerID);
 
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_SRGB8_ALPHA8, m_width, m_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_buffer);
 	glBindTexture(GL_TEXTURE_2D, 0);
@@ -26,13 +34,13 @@ Texture::Texture(const char* path)
 
 Texture::~Texture()
 {
-	glDeleteTextures(1, &m_id);
+	glDeleteTextures(1, &m_textureID);
 }
 
 void Texture::bind(unsigned int slot) const
 {
 	glActiveTexture(GL_TEXTURE0 + slot);
-	glBindTexture(GL_TEXTURE_2D, m_id);
+	glBindTexture(GL_TEXTURE_2D, m_textureID);
 }
 
 void Texture::unbind() const
--- Penguin/src/Texture.h
+++ Penguin/src/Texture.h
@@ -9,7 +9,8 @@ public:
 	void unbind() const;
 
 private:
-	unsigned int m_id;
+	unsigned int m_textureID;
+	unsigned int m_samplerID;
 	int m_width;
 	int m_height;
 	int m_bitsPerPixel;
